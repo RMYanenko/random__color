d8471e98c8d054068adfd076416c739a850fa879
RMYanenko <yanenko.r.m@gmail.com>
2020-09-10 15:31:50 +0300

    Home page

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
-import 'dart:math';
 import 'package:flutter/material.dart';
+import 'package:random_color/home-page.dart';
 
 void main() {
   runApp(MyApp());
@@ -14,49 +14,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blueGrey,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyHomePage(),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  Random _random = new Random();
-  Color _color = Color(0xFFFFFFFF);
-
-  void randomColor() {
-    setState(() {
-      _color = Color(_random.nextInt(0xffffffff));
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: InkWell(
-        onTap: randomColor,
-        child: Container(
-          decoration: new BoxDecoration(color: _color),
-          child: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Text(
-                  'Hey there',
-                  style: TextStyle(
-                    fontSize: 35.0,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-              ],
-            ),
-          ),
-        ),
-      ),
+      home: MyHomePage(title: 'Random Colors'),
     );
   }
 }
